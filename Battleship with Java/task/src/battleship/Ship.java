7f8fd1a7a91c61742ade894135585852f2405cb8
Nick Jones <147700520+JonesInForCode@users.noreply.github.com>
2024-05-24 04:24:32 -0700

    Added multiple pre-defined sets of ships.

--- Battleship with Java/task/src/battleship/Ship.java
+++ Battleship with Java/task/src/battleship/Ship.java
@@ -4,21 +4,66 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Ship {
-    private String type;
-    private int length;
+    public enum ShipType {
+        AIRCRAFT_CARRIER("Aircraft Carrier", 5),
+        BATTLESHIP("Battleship", 4),
+        SUBMARINE("Submarine", 3),
+        CRUISER("Cruiser", 3),
+        DESTROYER("Destroyer", 2);
+
+        private final String type;
+        private final int length;
+
+        ShipType(String type, int length) {
+            this.type = type;
+            this.length = length;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public int getLength() {
+            return length;
+        }
+    }
+
+    private ShipType type;
     private List<Coordinate> coordinates;
     private int hits;
     private boolean sunk;
 
-    public Ship(String type, int length) {
+    public Ship(ShipType type) {
         this.type = type;
-        this.length = length;
         this.coordinates = new ArrayList<>();
         this.hits = 0;
         this.sunk = false;
     }
     public void placeShip(List<Coordinate> placementCoordinates) {
         this.coordinates.addAll(placementCoordinates);
+    }
+
+    public ShipType getType() {
+        return type;
+    }
+
+    public int getLength() {
+        return type.getLength();
+    }
+
+    public List<Coordinate> getCoordinates() {
+        return coordinates;
+    }
+
+    public void hit() {
+        hits++;
+        if (hits >= type.getLength()) {
+            sunk = true;
+        }
+    }
+
+    public boolean isSunk() {
+        return sunk;
     }
 
     }
